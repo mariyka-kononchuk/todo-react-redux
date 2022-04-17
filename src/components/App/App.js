11ac0d74548ea9482193597382f568c4982c1733
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2022-04-17 17:22:21 +0300

    gh-pages works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import { Routes, Route, Navigate} from 'react-router-dom';
 import Container from '../Container';
 import HomeView from '../../views/HomeView';
@@ -7,13 +7,11 @@ import ArchiveView from '../../views/ArchiveView';
 export default function App() {
     return (
       <Container>
-        {/* <Suspense> */}
           <Routes>
               <Route path="/" element={<Navigate replace to="/home" />} />
               <Route path="/home" element={<HomeView />}/>
               <Route path="/archive" element={<ArchiveView />}/>
           </Routes>
-        {/* </Suspense> */}
       </Container>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize/modern-normalize.css';
@@ -13,9 +13,9 @@ render(
   <React.StrictMode>
     <Provider store={store.store}>
       <PersistGate loading={null} persistor={store.persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
     
